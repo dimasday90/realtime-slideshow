e23c42775e0802ca2cfada68ed09147eed35db72
Dimas Agung Yudono <dimasday90@gmail.com>
2020-02-20 19:31:07 +0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const PORT = 3000
+
+app.use(cors())
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json('hai')
+})
+
+io.on('connection', socket => {
+    console.log(`jamaah ${socket.id} hadir`)
+    socket.on('getData', () => {
+        io.emit('sendData', 'parsel')
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`sedekah ${PORT} dapat rezeki ${PORT*10}`)
+})
